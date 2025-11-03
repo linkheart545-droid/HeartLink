a2ed5448ee9b808250e18347fc0703db714f9b3c
abhinav <abhinav.mahalwal@constems-ai.com>
2025-11-03 13:16:49 +0530

    Initial commit 2

--- src/controller/RoomController.ts
+++ src/controller/RoomController.ts
@@ -1,12 +1,6 @@
 import {Request, Response} from "express"
-import {User} from "../model/User"
-import {DeleteObjectCommand, GetObjectCommand, PutObjectCommand} from "@aws-sdk/client-s3"
-import crypto from 'crypto'
-import {getSignedUrl} from "@aws-sdk/s3-request-presigner"
-import client from "../util/s3Client"
 import {randomInt} from "node:crypto";
 import {Room} from "../model/Room";
-import RoomRoutes from "../routes/RoomRoutes";
 
 const randomCodeGenerator = randomInt(0,100000)
 
@@ -36,7 +30,28 @@ const generateCode = async (req: Request, res: Response) => {
 }
 
 const updateCode = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
 
+        let code = randomCodeGenerator
+        let exists = await Room.exists({code : code })
+
+        while (exists) {
+            code = randomCodeGenerator
+            exists = await Room.exists({code : code})
+        }
+
+        const room = new Room({
+            code : code,
+            userId1 : userId,
+            userId2 : 0,
+        })
+
+        const savedRoom = await room.save()
+        res.status(201).json({room: savedRoom})
+    } catch (error: any) {
+        res.status(500).json({error: error.message})
+    }
 }
 
 export default {
